d85af820ce3a629f53b67668054fcf78c080d409
“Morskyi <normattt82@gmail.com>
2024-10-14 11:09:59 +0200

    initial commit
    (cherry picked from commit b0c5b4ee5af7d2ec76ec59d26307831dd683e571)

---
+++ src/main/java/com/epam/training/student_Yurii_Morskyi/fundamental/optional_task1/Main.java
@@ -0,0 +1,7 @@
+package com.epam.training.student_Yurii_Morskyi.fundamental.optional_task1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
